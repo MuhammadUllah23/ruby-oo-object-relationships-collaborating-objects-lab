e8cb347a2d7a7f9d8d80d2cb858e0129f562a137
Muhammad <muhammadhullah23@gmail.com>
2021-03-20 20:09:58 -0400

    commit tree 58cb7e6fd4ef0fdc3e996617a33d0b4cc44be377

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require "pry"
+class Artist   
+    attr_accessor :name
+    @@all = [] 
+    def initialize(name)
+        @name = name
+        @songs = []
+        @@all << self
+    end
+
+   
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+       
+       @songs << song
+        song.artist = self   
+    end
+    
+    def self.find_or_create_by_name(name)
+        self.all.detect {|artist| artist.name == name} || Artist.new(name)
+    end
+
+    def print_songs
+        @songs.each {|song| puts song.name}
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+    
+    
+end
